458480c8db7dff5d44cb6c257e29331b88afd3ad
naeka-k <kaNae@Andromeda>
2018-05-11 20:19:11 +0900

    test

---
+++ src/WhiteMain.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+/**
+ * @author kaNae
+ *
+ */
+public class WhiteMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
