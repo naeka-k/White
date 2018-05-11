a5e4906c339a66c030ae31641328c99bed621f19
naeka-k <kaNae@Andromeda>
2018-05-11 20:38:57 +0900

    test5

---
+++ src/nae/ka/test/WhiteClazz.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package nae.ka.test;
+
+/**
+ * @author naeka-k
+ *
+ */
+public class WhiteClazz {
+
+	private String name;
+
+	public WhiteClazz(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+}
