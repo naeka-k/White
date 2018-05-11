61f8be6ebb912f8e21b2727ec2ed83dd141eb5f6
naeka-k <39188574+naeka-k@users.noreply.github.com>
2018-05-11 20:52:55 +0900

    Update WhiteMain.java

--- src/nae/ka/test/WhiteMain.java
+++ src/nae/ka/test/WhiteMain.java
@@ -15,7 +15,7 @@ public class WhiteMain {
 	 */
 	public static void main(String[] args) {
 
-		WhiteClazz white = new WhiteClazz("siro");
+		WhiteClazz white = new WhiteClazz("SHIRO");
 		System.out.println(white.getName());
 	}
 
